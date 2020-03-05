743b6bd8c6bcd4ebb4821d411a140c22cfcadf88
[디지캡]명성 <smyeong@digicaps.com>
2020-03-05 15:09:58 +0900

    반응형 웹앱 Init

---
+++ responsive/src/common/configuration.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // 변경 시 src/common/styles.scss의 breakpoints도 변경 할 것.
+    windowSize: {   // 단위 px
+        small: 600,
+        medium: 768,
+        large: 992,
+        huge: 1200
+    }
+}
