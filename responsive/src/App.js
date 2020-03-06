b915504244e0ee26153ac166b3b7a607d0b252e9
[디지캡]명성 <smyeong@digicaps.com>
2020-03-06 18:17:51 +0900

    Header, Footer Add to Home Menu

--- responsive/src/App.js
+++ responsive/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 
 import CardList from './components/grid/cardList';
 import CardItemDetail from './components/detail/cardItemDetail';
+import TopMenu from './components/menu/TopMenu';
+import BottomMenu from './components/menu/BottomMenu';
 
 function App() {
   return (
     <div>
+      <TopMenu />
       <CardList />
       <CardList />
       <CardItemDetail />
+      <BottomMenu />
     </div>
   );
 }
