547a0618c9297b65f5eb08807c5ac859004f716b
westkite1201 <westkite1201@gmail.com>
2020-03-06 15:53:59 +0900

    cardItem style 이슈 해결, redux-saga back-end 연결

--- responsive/src/App.js
+++ responsive/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import CardList from './components/grid/cardList';
+import CardList from './components/grid/CardList';
 
 function App() {
   return (
--- responsive/src/components/grid/cardList.js
+++ responsive/src/components/grid/cardList.js
@@ -1,94 +1,96 @@
 import React, { useState, useCallback, useEffect } from 'react';
-
-import CardItem from './cardItem';
-import DummyCardItems from '../../common/dummyCardItems.json'
-
+import { useDispatch, useSelector } from 'react-redux';
+import { POST_REQUEST } from '../../reducer/post';
+import CardItem from './CardItem';
+import DummyCardItems from '../../common/dummyCardItems.json';
 import { makeStyles } from '@material-ui/core/styles';
 import GridList from '@material-ui/core/GridList';
-import GridListTile from '@material-ui/core/GridListTile';
-import GridListTileBar from '@material-ui/core/GridListTileBar';
-
 import useEventListener from '../../common/useEventListener';
-
 import configuration from '../../common/configuration';
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-        backgroundColor: theme.palette.background.paper,
-    },
-    gridList: {
-        flexWrap: 'nowrap',
-        // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
-        transform: 'translateZ(0)',
-    },
-    title: {
-        color: theme.palette.primary.light,
-    },
-    titleBar: {
-        background:
-            'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
-    },
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+    height: '300px',
+  },
+  gridList: {
+    flexWrap: 'nowrap',
+    // Promote the list into his own layer on Chrome. This cost memory but helps keeping high FPS.
+    transform: 'translateZ(0)',
+    height: '100%',
+  },
+  gridListLi: {
+    height: '100% !important',
+  },
+  title: {
+    color: 'white',
+    fontSize: '1.2rem',
+  },
+  titleBar: {
+    background:
+      'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
+  },
 }));
 
-const CardList = (props) => {
-    ////////////////////
-    //< Constant
-    const [cardItems, setCardItems] = useState([]);
-    const [windowSize, setWindowSize] = useState(0);
-    const classes = useStyles();
-    ////////////////////
-    //< React LifeCycle
-    useEffect(() => {
-        setCardItems(DummyCardItems);
-        setWindowSize(window.innerWidth);
-    }, []);
+const CardList = props => {
+  ////////////////////
+  //< Constant
+  const dispatch = useDispatch();
+  const { cardData } = useSelector(state => state.post);
 
-    ////////////////////
-    //< Handler (include focus manager)
-    const windowSizeHandler = useCallback(() => {
-        let windowWidth = typeof window !== "undefined" ? window.innerWidth : 0;
-        let windowHeight = typeof window !== "undefined" ? window.innerHeight : 0;
+  const [cardItems, setCardItems] = useState([]);
+  const [windowSize, setWindowSize] = useState(0);
+  const classes = useStyles();
 
-        setWindowSize(windowWidth);
-    })
+  ////////////////////
+  //< React LifeCycle
+  useEffect(() => {
+    dispatch({
+      type: POST_REQUEST,
+    });
+    setCardItems(DummyCardItems);
+    setWindowSize(window.innerWidth);
+  }, []);
 
-    ////////////////////
-    //< private functions
+  ////////////////////
+  //< Handler (include focus manager)
+  const windowSizeHandler = useCallback(() => {
+    let windowWidth = typeof window !== 'undefined' ? window.innerWidth : 0;
+    let windowHeight = typeof window !== 'undefined' ? window.innerHeight : 0;
+    setWindowSize(windowWidth);
+  });
 
-    ////////////////////
-    //< Rendering
-    useEventListener('resize', windowSizeHandler);
-    const colsSize = windowSize > configuration.windowSize.small ? 6 : 3;
+  ////////////////////
+  //< private functions
 
-    return (
-        <div className={classes.root}>
-            <GridList className={classes.gridList} cols={colsSize}>
-                {cardItems.map(tile => (
-                    <GridListTile key={tile.img}>
-                        <img src={tile.img} alt={tile.title} height='100%' />
-                        {/* <GridListTileBar
-                            title={tile.title}
-                            classes={{
-                                root: classes.titleBar,
-                                title: classes.title,
-                            }}
-                        /> */}
-                    </GridListTile>
-                ))}
-            </GridList>
+  console.log(cardData);
+  ////////////////////
+  //< Rendering
+  useEventListener('resize', windowSizeHandler);
+  const colsSize = windowSize > configuration.windowSize.small ? 6 : 3;
+  return (
+    <React.Fragment>
+      <div className={classes.root}>
+        <GridList className={classes.gridList} cols={colsSize}>
+          {DummyCardItems.map((tile, key) => (
+            <CardItem key={key} cardItem={tile} classes={classes} />
+          ))}
+        </GridList>
+      </div>
 
-
-            {/* <GridList className={classes.gridList} cols={colsSize}>
-                {cardItems.map(cardItem => (
-                    <CardItem cardItem={cardItem} />    CardItem - CardList 분리작업. 적용되지 않았음.
-                ))}
-            </GridList> */}
-        </div>
-    )
-}
+      <div className={classes.root}>
+        <GridList className={classes.gridList} cols={colsSize}>
+          {cardData.map((tile, key) => (
+            <CardItem key={key} cardItem={tile} classes={classes} />
+          ))}
+        </GridList>
+      </div>
+    </React.Fragment>
+  );
+};
 
 export default CardList;
