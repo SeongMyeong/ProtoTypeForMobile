5d10429e77a9082e506c09f1442c4460a8d7fb31
[디지캡]명성 <smyeong@digicaps.com>
2020-03-10 10:17:18 +0900

    Footer ui

--- responsive/src/components/Footer/Footer.js
+++ responsive/src/components/Footer/Footer.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, fade } from '@material-ui/core/styles';
 import BottomNavigation from '@material-ui/core/BottomNavigation';
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
 import RestoreIcon from '@material-ui/icons/Restore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 
-const useStyles = makeStyles({
+import useEventListener from '../../common/useEventListener';
+
+const useStyles = makeStyles(theme => ({
   root: {
     zIndex: 0,
     width: '100%',
     height: '50px',
     maxHeight: '1200px',
-    background: 'grey',
     bottom: '0',
-    background: 'transparent',
-    color: 'white',
     position: 'fixed',
     transition: 'all 1s',
+    backgroundColor: 'white'
   }
-});
+}));
 
 const Footer = props => {
 
   const classes = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
+  // const [isBottomNavigationView, setIsBottomNavigationView] = useState(true)
+
+  // private function
+  // const windowScrollHandler = useCallback(() => {
+  //   let windowScrollY = typeof window !== 'undefined' ? window.scrollY : 0;
+  //   if (windowScrollY < 100) {
+  //     setIsBottomNavigationView(false);
+  //   } else {
+  //     setIsBottomNavigationView(true);
+  //   }
+  // }, [window.scrollY]);
+
+  // useEventListener('scroll', windowScrollHandler);
 
   return (
     <BottomNavigation
@@ -35,6 +48,7 @@ const Footer = props => {
       }}
       showLabels
       className={classes.root}
+    // style={{ display: isBottomNavigationView ? 'flex' : 'none' }}
     >
       <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
       <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
@@ -44,51 +58,3 @@ const Footer = props => {
 };
 
 export default Footer;
-
-
-/*
-import React from 'react';
-
-import { makeStyles } from '@material-ui/core/styles';
-import BottomNavigation from '@material-ui/core/BottomNavigation';
-import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationOnIcon from '@material-ui/icons/LocationOn';
-
-const useStyles = makeStyles({
-    root: {
-        zIndex: 0,
-        width: '100%',
-    height: '50px',
-    background: 'grey',
-    bottom: '0',
-    position: 'fixed'
-  }
-    },
-});
-
-const BottomMenu = props => {
-
-    const classes = useStyles();
-    const [value, setValue] = React.useState(0);
-
-    return (
-        <BottomNavigation
-            value={value}
-            onChange={(event, newValue) => {
-                setValue(newValue);
-            }}
-            showLabels
-            className={classes.root}
-        >
-            <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
-            <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
-            <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
-        </BottomNavigation>
-    );
-};
-
-export default BottomMenu;
-
-*/
